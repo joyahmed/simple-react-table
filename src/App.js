8789242b759126a90503869334fcc287371fd0fc
joyahmed <joy_ahmed_007@yahoo.com>
2021-07-16 22:15:51 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Table from './Table';
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {
+		characters: []
+	};
+
+	removeCharacter = index => {
+		const { characters } = this.state;
+
+		this.setState({
+			characters: characters.filter((character, i) => {
+				return i !== index;
+			})
+		});
+	};
+
+	handleSubmit = character => {
+		this.setState({ characters: [...this.state.characters, character] });
+	};
+
+	render() {
+		const { characters } = this.state;
+
+		return (
+			<div className="container">
+				<p style={{ paddingTop: '50px' }}>
+					Add a character with a name and a job to the table.
+				</p>
+				<Table
+					characterData={characters}
+					removeCharacter={this.removeCharacter}
+				/>
+				<h3>Add New</h3>
+				<Form handleSubmit={this.handleSubmit} />
+			</div>
+		);
+	}
 }
 
 export default App;
